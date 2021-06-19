f638c3cf13b3f3f6b07398cfe810e39447143564
Thorsten Klöhn <thorstenkloehn@gmail.com>
2021-06-19 04:02:46 +0200

    Test

--- pull.sh
+++ pull.sh
@@ -4,7 +4,9 @@ export PATH=$GOPATH/bin:$HOME/.cargo/bin:$GOROOT/bin:$PATH
 cd /Server
 git pull
 git submodule update --recursive --remote
+ cd /Server/ahrensburg.digital
 go build
+cd /Server/dokument
  sudo /root/.composer/vendor/daux/daux.io/bin/daux generate
 
 sudo  systemctl stop ahrensburg.service
